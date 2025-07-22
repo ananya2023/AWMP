d4824afcc87869b99f89573960f47e0a8edacfa3
kruti-gnapitech <kruti.patel@gnapitech.com>
2025-07-22 20:05:25 +0530

    backend added

---
+++ Backend/src/exceptions/HTTPBadRequest.js
@@ -0,0 +1,19 @@
+/**
+* HTTPBadRequest
+* 
+* @author Vikas Sood <vikas@giskernel.com>
+* @returns {Object}
+* @name HTTPBadRequest
+* @alias HTTPBadRequest Exception
+* @param {Null}
+* 
+*/
+const HTTPError = require('./HTTPError')
+
+class HTTPBadRequest extends HTTPError {
+    constructor(message = 'Bad Request') {
+        super(400, message)
+    }
+}
+
+module.exports = HTTPBadRequest
